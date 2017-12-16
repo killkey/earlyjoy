f92674fdb57dcdbf26a84a0d9f20a517b4612986
gaoxing88 <943016287@qq.com>
2017-12-16 17:34:16 +0800

    rank.js

--- client/container/rank/rank.jsx
+++ client/container/rank/rank.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-
+import {ajax} from "../../util/index";
+import {HashRouter as Router,Route,Link} from 'react-router-dom'
+import index from '../index/index'
 export default class extends Component {
   constructor () {
     super();
@@ -9,14 +11,37 @@ export default class extends Component {
   }
 
   componentWillMount () {
-
+    ajax({
+      method:'post',
+      url:'http://localhost:8333/api/ranklist',
+      data:{offset:0,limit:10}
+    }).then(res=>{
+      let {list,hasMore}=res;
+      this.setState({list,hasMore})
+    }).catch(err=>{
+      console.log(err);
+    })
   }
 
   render () {
     return (
+      <Router>
       <div className="page-wrap rank-page">
-        排行榜
+        <Route path='/index' component={index}/>
+        {
+          this.state.hasMore?this.state.list.map((item,index)=>(
+            <Link to='/detail' className='rank-item' key={index}>
+              <div className="rank-level">{item.rank}</div>
+              <div className="rank-avatar">
+                <img src={item.avatar} alt=""/>
+              </div>
+              <span className="rank-user-name">{item.userName}</span>
+              <div className="rank-continued">{`连续起床${item.continued}天`}</div>
+            </Link>
+          )):null
+        }
       </div>
+      </Router>
     )
   }
 }
