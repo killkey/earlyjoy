a37a56e3d3d4152ee11bb559799f55f979e92f91
duyi0518 <423403391@qq.com>
2017-12-16 21:16:39 +0800

    Home

--- client/container/index/index.jsx
+++ client/container/index/index.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import actions from '../../redux/actions/index.js';
-
+import {ajax} from '../../util/index';
+import {Link} from 'react-router-dom'
 @connect((state) => {
   return {
     cntData: state.cnt
@@ -10,28 +11,62 @@ import actions from '../../redux/actions/index.js';
 export default class extends Component {
   constructor () {
     super();
-    this.state = {
-
-    };
+    this.state = {list:[]};
   }
 
-  clickHandler () {
-    this.props.setCnt();
+  handleClick=()=> {
+    this.props.history.push('/rank')
+  }
+
+  componentDidMount(){
+    ajax({
+      url:'http://localhost:8333/api/myinfo',
+      method:'get'
+    }).then(res=>{
+
+      let {userName,avatar}=res;
+      this.setState({userName,avatar});
+    }).catch(err=>{
+
+    }),
+      ajax({
+        url:'http://localhost:8333/api/mylist',
+        method:'post',
+        data:{offset:0,limit:2}
+      }).then(res=>{
+        let {list}=res;
+        this.setState({list});
+      }).catch(err=>{
+
+      })
   }
 
 
   render () {
     let { cnt } = this.props.cntData;
-
+    let {userName,avatar}=this.state;
+    let {img,text,getupTime}=this.state;
     return (
         <div className="page-wrap main-page" ref="mainPage">
-          首页
-          <br/>
-          <span>使用 react global state(redux store) 切换路由后数据还在</span>
-          <br/>
-          <button onClick={this.clickHandler.bind(this)}>点击数量加一</button>
-          <br/>
-          <span>数量：</span><span>{cnt}</span>
+          <div className="user-info-wrap">
+            <img className="user-avatar" src={avatar}/>
+            <p className='user-name'>{userName}</p>
+            <div onClick={this.handleClick} className="today">
+              <i>+</i><span>添加今日状态</span>
+            </div>
+
+            {
+              this.state.list.map((item,index)=>(
+                <Link to={`/detail/$(item.uid)`} key={index} className="box">
+                  <img className="picture" src={item.img}/>
+                  <p className="new">{item.text}</p>
+                  <p className="data">{item.time}</p>
+                </Link>
+              ))
+            }
+
+
+          </div>
         </div>
     )
   }
