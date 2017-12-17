6243c4b69cc3d08dd8f62f7c9fb1da3f9f2ee7da
bestzzz <707098011@qq.com>
2017-12-17 09:39:49 +0800

    init

--- client/container/today/today.jsx
+++ client/container/today/today.jsx
@@ -1,34 +1,55 @@
 import React, { Component } from 'react';
+import {ajax} from "../../util/index";
+import {Link} from "react-router-dom";
 
 export default class extends Component {
   constructor () {
     super();
     this.state = {
-      cnt: 0
+      list: []
     };
   }
 
   componentWillMount () {
-
-  }
-
-  clickHandler () {
-    this.setState({
-      cnt: this.state.cnt+1
-    })
+    //{ method='', url='', async=true, data={}, headers={} }
+    ajax({
+      method: 'POST',
+      url: 'http://localhost:8333/api/todaylist',
+      data: {
+        offset: 0,
+        limit: 24,
+      }
+    }).then(res => {
+      let {list, hasMore} = res;
+      this.setState({list, hasMore});
+    }).catch(error => {
+      console.log('请求/api/todaylist失败');
+    });
   }
 
   render () {
-    let { cnt } = this.state;
 
     return (
       <div className="page-wrap today-page" ref="todayPage">
-        今日列表
-        <br/>
-        <span>使用 react local state 切换路由后数据就不见了</span>
-        <button onClick={this.clickHandler.bind(this)}>点击数量加一</button>
-        <br/>
-        <span>数量：</span><span>{cnt}</span>
+        今日列表好友情况/好友列表
+        <ul className='today-list'>
+          {
+            this.state.list.map((item, index) => (
+              <Link to={`/detail/${item.uid}`} key={index}>
+                <li>
+                  <img src={item.img} alt=""/>
+                  <p>
+                    <span className='today-list-name'>{item.userName}</span>
+                    <span className='today-list-reason'>起床迟到</span>
+                    <span className='today-list-date'>{item.time}</span>
+                  </p>
+                  <span className='today-list-detail'>{item.text}</span>
+                  <span className='today-list-heart'>i</span>
+                </li>
+              </Link>
+            ))
+          }
+        </ul>
       </div>
     )
   }
